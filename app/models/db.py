0e5859ad672a0db8a9cb73a47e095a685198e8d2
Shivansh Pareek <shivanshpareek@Shivanshs-MacBook-Pro.local>
2025-12-14 02:59:52 +0530

    Added vector extension to docker image and pulled old one with vector db support and tested with 384 standard dimesnion to see chunks being  updated in db

--- app/models/db.py
+++ app/models/db.py
@@ -1,7 +1,17 @@
-from sqlalchemy import create_engine, Column, String, DateTime
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import (
+    create_engine,
+    Column,
+    String,
+    DateTime,
+    Integer,
+    ForeignKey,
+    Text,
+)
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 from datetime import datetime
 from app.config import DATABASE_URL
+
+from pgvector.sqlalchemy import Vector
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
@@ -15,6 +25,22 @@ class Document(Base):
     filename = Column(String, nullable=False)
     status = Column(String, default="uploaded")
     uploaded_at = Column(DateTime, default=datetime.utcnow)
+
+    chunks = relationship("DocumentChunk", back_populates="document")
+
+
+class DocumentChunk(Base):
+    __tablename__ = "document_chunks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    document_id = Column(String, ForeignKey("documents.id"))
+    chunk_index = Column(Integer)
+    chunk_text = Column(Text)
+
+    embedding = Column(Vector(384))  # sentence-transformers dimension
+
+    document = relationship("Document", back_populates="chunks")
+
 
 def init_db():
     Base.metadata.create_all(bind=engine)
